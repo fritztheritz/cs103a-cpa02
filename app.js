c6536f145cfb7ec1c1aba07e91b839def4dfc402
fritztheritz <fduverglas@brandeis.edu>
2022-04-25 13:41:53 -0400

    small changes

--- app.js
+++ app.js
@@ -321,7 +321,8 @@ app.set("port", port);
 const http = require("http");
 const server = http.createServer(app);
 
-server.listen(port);
+// server.listen(port);
+server.listen(process.env.PORT || 5000);
 
 function onListening() {
     var addr = server.address();
